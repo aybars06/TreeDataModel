6b866a6131fb9894ad728666ff6e7e8a4196c56b
Aybars <132859590+aybars06@users.noreply.github.com>
2024-12-08 20:44:57 +0300

    Add files via upload

---
+++ Ağaç Veri Modeli Projesi/Ağaç Veri Modeli Projesi/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace Ağaç_Veri_Modeli_Projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.yapraklartxtBox = new System.Windows.Forms.TextBox();
+            this.yuksekliktxtBox = new System.Windows.Forms.TextBox();
+            this.dugumsayisitxtBox = new System.Windows.Forms.TextBox();
+            this.postordertxtBox = new System.Windows.Forms.TextBox();
+            this.inordertxtBox = new System.Windows.Forms.TextBox();
+            this.preordertxtBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.agacbilgileri_btn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dugumekletxtBox = new System.Windows.Forms.TextBox();
+            this.dugumsiltxtBox = new System.Windows.Forms.TextBox();
+            this.dugumduzeytxtBox = new System.Windows.Forms.TextBox();
+            this.dugumgoster_btn = new System.Windows.Forms.Button();
+            this.dugumgostertxtBox = new System.Windows.Forms.TextBox();
+            this.dugumekle_btn = new System.Windows.Forms.Button();
+            this.dugumsil_btn = new System.Windows.Forms.Button();
+            this.dugumbul_btn = new System.Windows.Forms.Button();
+            this.ekleLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.yapraklartxtBox);
+            this.groupBox1.Controls.Add(this.yuksekliktxtBox);
+            this.groupBox1.Controls.Add(this.dugumsayisitxtBox);
+            this.groupBox1.Controls.Add(this.postordertxtBox);
+            this.groupBox1.Controls.Add(this.inordertxtBox);
+            this.groupBox1.Controls.Add(this.preordertxtBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(439, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(332, 340);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ağaç Bilgileri";
+            // 
+            // yapraklartxtBox
+            // 
+            this.yapraklartxtBox.Location = new System.Drawing.Point(92, 271);
+            this.yapraklartxtBox.Name = "yapraklartxtBox";
+            this.yapraklartxtBox.Size = new System.Drawing.Size(234, 22);
+            this.yapraklartxtBox.TabIndex = 11;
+            // 
+            // yuksekliktxtBox
+            // 
+            this.yuksekliktxtBox.Location = new System.Drawing.Point(141, 227);
+            this.yuksekliktxtBox.Name = "yuksekliktxtBox";
+            this.yuksekliktxtBox.Size = new System.Drawing.Size(185, 22);
+            this.yuksekliktxtBox.TabIndex = 10;
+            // 
+            // dugumsayisitxtBox
+            // 
+            this.dugumsayisitxtBox.Location = new System.Drawing.Point(226, 180);
+            this.dugumsayisitxtBox.Name = "dugumsayisitxtBox";
+            this.dugumsayisitxtBox.Size = new System.Drawing.Size(100, 22);
+            this.dugumsayisitxtBox.TabIndex = 9;
+            // 
+            // postordertxtBox
+            // 
+            this.postordertxtBox.Location = new System.Drawing.Point(107, 121);
+            this.postordertxtBox.Name = "postordertxtBox";
+            this.postordertxtBox.Size = new System.Drawing.Size(219, 22);
+            this.postordertxtBox.TabIndex = 8;
+            // 
+            // inordertxtBox
+            // 
+            this.inordertxtBox.Location = new System.Drawing.Point(107, 81);
+            this.inordertxtBox.Name = "inordertxtBox";
+            this.inordertxtBox.Size = new System.Drawing.Size(219, 22);
+            this.inordertxtBox.TabIndex = 7;
+            // 
+            // preordertxtBox
+            // 
+            this.preordertxtBox.Location = new System.Drawing.Point(107, 44);
+            this.preordertxtBox.Name = "preordertxtBox";
+            this.preordertxtBox.Size = new System.Drawing.Size(219, 22);
+            this.preordertxtBox.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(203, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ağaçtaki Toplam Düğüm Sayısı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Yapraklar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ağacın Yüksekliği";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Postorder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Inorder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Preorder";
+            // 
+            // agacbilgileri_btn
+            // 
+            this.agacbilgileri_btn.Location = new System.Drawing.Point(445, 12);
+            this.agacbilgileri_btn.Name = "agacbilgileri_btn";
+            this.agacbilgileri_btn.Size = new System.Drawing.Size(326, 39);
+            this.agacbilgileri_btn.TabIndex = 1;
+            this.agacbilgileri_btn.Text = "Ağaç Bilgilerini Göster";
+            this.agacbilgileri_btn.UseVisualStyleBackColor = true;
+            this.agacbilgileri_btn.Click += new System.EventHandler(this.agacbilgileri_btn_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Düğüm Eklendi";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(37, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Düğüm Silindi";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 126);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 17);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Düğümün Düzeyi";
+            // 
+            // dugumekletxtBox
+            // 
+            this.dugumekletxtBox.Location = new System.Drawing.Point(146, 34);
+            this.dugumekletxtBox.Name = "dugumekletxtBox";
+            this.dugumekletxtBox.Size = new System.Drawing.Size(100, 22);
+            this.dugumekletxtBox.TabIndex = 5;
+            // 
+            // dugumsiltxtBox
+            // 
+            this.dugumsiltxtBox.Location = new System.Drawing.Point(146, 79);
+            this.dugumsiltxtBox.Name = "dugumsiltxtBox";
+            this.dugumsiltxtBox.Size = new System.Drawing.Size(100, 22);
+            this.dugumsiltxtBox.TabIndex = 6;
+            // 
+            // dugumduzeytxtBox
+            // 
+            this.dugumduzeytxtBox.Location = new System.Drawing.Point(146, 126);
+            this.dugumduzeytxtBox.Name = "dugumduzeytxtBox";
+            this.dugumduzeytxtBox.Size = new System.Drawing.Size(100, 22);
+            this.dugumduzeytxtBox.TabIndex = 7;
+            // 
+            // dugumgoster_btn
+            // 
+            this.dugumgoster_btn.Location = new System.Drawing.Point(28, 172);
+            this.dugumgoster_btn.Name = "dugumgoster_btn";
+            this.dugumgoster_btn.Size = new System.Drawing.Size(336, 36);
+            this.dugumgoster_btn.TabIndex = 8;
+            this.dugumgoster_btn.Text = "Ağaçtaki Düğümleri Göster";
+            this.dugumgoster_btn.UseVisualStyleBackColor = true;
+            this.dugumgoster_btn.Click += new System.EventHandler(this.dugumgoster_btn_Click);
+            // 
+            // dugumgostertxtBox
+            // 
+            this.dugumgostertxtBox.Location = new System.Drawing.Point(46, 214);
+            this.dugumgostertxtBox.Multiline = true;
+            this.dugumgostertxtBox.Name = "dugumgostertxtBox";
+            this.dugumgostertxtBox.Size = new System.Drawing.Size(304, 224);
+            this.dugumgostertxtBox.TabIndex = 9;
+            // 
+            // dugumekle_btn
+            // 
+            this.dugumekle_btn.Location = new System.Drawing.Point(275, 18);
+            this.dugumekle_btn.Name = "dugumekle_btn";
+            this.dugumekle_btn.Size = new System.Drawing.Size(133, 48);
+            this.dugumekle_btn.TabIndex = 12;
+            this.dugumekle_btn.Text = "Düğüm Ekleme";
+            this.dugumekle_btn.UseVisualStyleBackColor = true;
+            this.dugumekle_btn.Click += new System.EventHandler(this.dugumekle_btn_Click);
+            // 
+            // dugumsil_btn
+            // 
+            this.dugumsil_btn.Location = new System.Drawing.Point(275, 71);
+            this.dugumsil_btn.Name = "dugumsil_btn";
+            this.dugumsil_btn.Size = new System.Drawing.Size(133, 47);
+            this.dugumsil_btn.TabIndex = 13;
+            this.dugumsil_btn.Text = "Düğüm Silme";
+            this.dugumsil_btn.UseVisualStyleBackColor = true;
+            this.dugumsil_btn.Click += new System.EventHandler(this.dugumsil_btn_Click);
+            // 
+            // dugumbul_btn
+            // 
+            this.dugumbul_btn.Location = new System.Drawing.Point(275, 124);
+            this.dugumbul_btn.Name = "dugumbul_btn";
+            this.dugumbul_btn.Size = new System.Drawing.Size(133, 49);
+            this.dugumbul_btn.TabIndex = 14;
+            this.dugumbul_btn.Text = "Düğüm Bulma";
+            this.dugumbul_btn.UseVisualStyleBackColor = true;
+            this.dugumbul_btn.Click += new System.EventHandler(this.dugumbul_btn_Click);
+            // 
+            // ekleLabel
+            // 
+            this.ekleLabel.AutoSize = true;
+            this.ekleLabel.Location = new System.Drawing.Point(3, 37);
+            this.ekleLabel.Name = "ekleLabel";
+            this.ekleLabel.Size = new System.Drawing.Size(28, 17);
+            this.ekleLabel.TabIndex = 15;
+            this.ekleLabel.Text = "----";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ekleLabel);
+            this.Controls.Add(this.dugumekle_btn);
+            this.Controls.Add(this.dugumsil_btn);
+            this.Controls.Add(this.dugumbul_btn);
+            this.Controls.Add(this.dugumgostertxtBox);
+            this.Controls.Add(this.dugumgoster_btn);
+            this.Controls.Add(this.dugumduzeytxtBox);
+            this.Controls.Add(this.dugumsiltxtBox);
+            this.Controls.Add(this.dugumekletxtBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.agacbilgileri_btn);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox yapraklartxtBox;
+        private System.Windows.Forms.TextBox yuksekliktxtBox;
+        private System.Windows.Forms.TextBox dugumsayisitxtBox;
+        private System.Windows.Forms.TextBox postordertxtBox;
+        private System.Windows.Forms.TextBox inordertxtBox;
+        private System.Windows.Forms.TextBox preordertxtBox;
+        private System.Windows.Forms.Button agacbilgileri_btn;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox dugumekletxtBox;
+        private System.Windows.Forms.TextBox dugumsiltxtBox;
+        private System.Windows.Forms.TextBox dugumduzeytxtBox;
+        private System.Windows.Forms.Button dugumgoster_btn;
+        private System.Windows.Forms.TextBox dugumgostertxtBox;
+        private System.Windows.Forms.Button dugumekle_btn;
+        private System.Windows.Forms.Button dugumsil_btn;
+        private System.Windows.Forms.Button dugumbul_btn;
+        private System.Windows.Forms.Label ekleLabel;
+    }
+}
+
---
+++ Ağaç Veri Modeli Projesi/Ağaç Veri Modeli Projesi/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ağaç_Veri_Modeli_Projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class agacyapisi
+        {
+            public agacyapisi sol;
+            public int veri;
+            public agacyapisi sag;
+        }
+        public agacyapisi kok = null;
+        int dugumsayisi = 0;
+        private void dugumekle_btn_Click(object sender, EventArgs e)
+        {
+            agacyapisi yeni = new agacyapisi();
+            yeni.veri = int.Parse(dugumekletxtBox.Text);
+            dugumekletxtBox.Clear();
+            //ağaca ekleme bölümü
+            if (kok == null)
+            {
+                kok = yeni;
+                kok.sag = null;
+                kok.sol = null;
+                dugumsayisi++;
+                ekleLabel.Text = " " + dugumsayisi.ToString() + ".";
+            }
+            else
+            {
+                agacyapisi kaydedilen = kok;
+                agacyapisi islenen = kok;
+                while (true)
+                {
+                    kaydedilen = islenen;
+                    if (kaydedilen.veri == yeni.veri)
+                    {
+                        MessageBox.Show("Daha Önceden Eklenen Veri Eklenemez", "HATA");
+                        break;
+                    }
+                    else if (kaydedilen.veri < yeni.veri)
+                    {
+                        islenen = islenen.sag;
+                        if (islenen == null)
+                        {
+                            kaydedilen.sag = yeni;
+                            kaydedilen = kaydedilen.sag;
+                            kaydedilen.sag = null;
+                            kaydedilen.sol = null;
+                            dugumsayisi++;
+                            ekleLabel.Text = " " + dugumsayisi.ToString() + ".";
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        islenen = islenen.sol;
+                        if (islenen == null)
+                        {
+                            kaydedilen.sol = yeni;
+                            kaydedilen = kaydedilen.sol;
+                            kaydedilen.sol = null;
+                            kaydedilen.sag = null;
+                            dugumsayisi++;
+                            ekleLabel.Text = " " + dugumsayisi.ToString() + ".";
+                            return;
+                        }
+
+                    }
+
+
+                }
+
+            }
+
+        }
+
+        private void preorderYazdir(agacyapisi yazılacak)
+        {
+            if (yazılacak == null)
+            {
+                return;
+            }
+            preordertxtBox.Text += yazılacak.veri.ToString() + "-->";
+            preorderYazdir(yazılacak.sol);
+            preorderYazdir(yazılacak.sag);
+        }
+        private void postorderYazdir(agacyapisi yazılacak)
+        {
+            if (yazılacak == null)
+            {
+                return;
+            }
+
+            postorderYazdir(yazılacak.sol);
+            postorderYazdir(yazılacak.sag);
+            postordertxtBox.Text += yazılacak.veri.ToString() + "-->";
+        }
+        private void inorderYazdir(agacyapisi yazılacak)
+        {
+            if (yazılacak == null)
+            {
+                return;
+            }
+
+            inorderYazdir(yazılacak.sol);
+            inordertxtBox.Text += yazılacak.veri.ToString() + "-->";
+            inorderYazdir(yazılacak.sag);
+
+        }
+        int dugumsirasi = 0;
+        private void dugumYazdir(agacyapisi yazılacak)
+        {
+            if (yazılacak == null)
+            {
+                return;
+            }
+            dugumsirasi++;
+            dugumgostertxtBox.Text += dugumsirasi + ".Dugum -->\t" + yazılacak.veri.ToString() + Environment.NewLine;
+            dugumYazdir(yazılacak.sol);
+            dugumYazdir(yazılacak.sag);
+        }
+        int aramasayac = 0;
+        private void dugumArama(agacyapisi root, int deger)
+        {
+            if (root == null)
+            {
+                aramasayac++;
+                MessageBox.Show("Aradığınız Değer Ağaç Yapısında Bulunamadı...", "BİLGİ");
+                aramasayac = 0;
+                return;
+            }
+            if (deger == root.veri)
+            {
+                aramasayac++;
+                MessageBox.Show("Aradığınız Değer Ağaç Yapısında Bulundu...\n\n\t    (" + aramasayac + ".Düzeyde Bulunuyor)", "BİLGİ");
+                aramasayac = 0;
+                return;
+            }
+            else if (deger < root.veri)
+            {
+                aramasayac++;
+                dugumArama(root.sol, deger);
+            }
+            else
+            {
+                aramasayac++;
+                dugumArama(root.sag, deger);
+            }
+        }
+        private agacyapisi dugumSil(agacyapisi kok, int gelen)
+        {
+            //silinecek ağaç yok.
+            if (kok == null)
+            {
+                MessageBox.Show("Ağaç Yapısında Bulunmuyor");
+                return kok;
+            }
+            //Altta düğüm olmayandan silme
+            if (gelen < kok.veri)     //kökün sürekli sol alt ağaçlarını kontrol et.
+            {
+                kok.sol = dugumSil(kok.sol, gelen);
+            }
+
+            else if (gelen > kok.veri) //kökün sürekli sağ alt ağaçlarını kontrol et.
+            {
+                kok.sag = dugumSil(kok.sag, gelen);
+            }
+            else
+            {
+                // sadece allta tek düğüm olandan silme
+                if (kok.sol == null)
+                {
+                    return kok.sag;
+                }
+                else if (kok.sag == null)
+                {
+                    return kok.sol;
+                }
+                // sadece altta iki düğüm bulunandan silme.
+                // sağ alt ağaçtaki min değeri al. 
+                kok.veri = minBul(kok.sag);
+                kok.sag = dugumSil(kok.sag, kok.veri);
+            }
+
+            return kok;
+        }
+        private void yaprakGoster(agacyapisi agac)
+        {
+            if (agac == null)
+            {
+                return;
+            }
+            if (agac.sag == null && agac.sol == null)
+            {
+                yapraklartxtBox.Text += agac.veri.ToString() + "-->";
+            }
+
+            yaprakGoster(agac.sol);
+            yaprakGoster(agac.sag);
+        }
+        private int yukseklikBul(agacyapisi kok)
+        {
+            if (kok == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int solYukseklik = yukseklikBul(kok.sol);
+                int sagYukseklik = yukseklikBul(kok.sag);
+                if (sagYukseklik >= solYukseklik)
+                {
+                    yuksekliktxtBox.Text = sagYukseklik.ToString();
+                    return (sagYukseklik + 1);
+                }
+                else
+                {
+                    yuksekliktxtBox.Text = solYukseklik.ToString();
+                    return (solYukseklik + 1);
+                }
+            }
+        }
+        private int maxBul(agacyapisi aranan)
+        {
+            int maxdeger = aranan.veri;
+            {
+                while (true)
+                {
+
+                    if (aranan.sag != null)
+                    {
+                        maxdeger = aranan.sag.veri;
+                        aranan = aranan.sag;
+
+                    }
+                    return maxdeger;
+                }
+            }
+        }
+        private int minBul(agacyapisi aranan)
+        {
+            int mindeger = aranan.veri;
+            {
+                while (aranan.sol != null)
+                {
+
+                    mindeger = aranan.sol.veri;
+                    aranan = aranan.sol;
+                }
+                return mindeger;
+            }
+        }
+        private void dugumsil_btn_Click(object sender, EventArgs e)
+        {
+            int silinecek = int.Parse(dugumsiltxtBox.Text);
+            dugumSil(kok, silinecek);
+        }
+
+        private void dugumbul_btn_Click(object sender, EventArgs e)
+        {
+            int aranan = int.Parse(dugumduzeytxtBox.Text);
+            dugumArama(kok, aranan);
+
+        }
+
+        private void dugumgoster_btn_Click(object sender, EventArgs e)
+        {
+            dugumgostertxtBox.Text = null;
+            dugumsirasi = 0;
+            dugumYazdir(kok);
+        }
+
+        private void agacbilgileri_btn_Click(object sender, EventArgs e)
+        {
+            preordertxtBox.Text = null;
+            inordertxtBox.Text = null;
+            postordertxtBox.Text = null;
+            yapraklartxtBox.Text = null;
+            preorderYazdir(kok);
+            inorderYazdir(kok);
+            postorderYazdir(kok);
+            dugumsayisitxtBox.Text = dugumsayisi.ToString();
+            yaprakGoster(kok);
+            yukseklikBul(kok);
+        }
+    }
+
+}
